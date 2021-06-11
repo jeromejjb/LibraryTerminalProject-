bfafc52766e4d6c3c5ce2f5a24b8d0f89343aeba
jerome <jeromebrown97@gmail.com>
2021-06-11 15:34:59 -0500

    Added Book class and book txt file

---
+++ LibraryTerminalProject/Books.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryTerminalProject
+{
+    class Books : Library
+    {
+     public string title 
+
+        public enum Category
+        {
+          
+        }
+        
+
+    }   
+}
